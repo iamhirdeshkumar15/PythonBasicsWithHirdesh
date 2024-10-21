2c2e0a7e345910a0b21629220f70721d8f741f08
Hirdesh <hirdeshk976@gmail.com>
2024-10-21 08:19:36 +0530

    in this file use print and declare variables

---
+++ 1st_Program.py
@@ -0,0 +1,24 @@
+print("Hirdsh is my name.","My age is 23.")
+print(23)
+
+
+print("Kumar is my name.")
+print("My age is 23.")
+
+print(35+23)
+
+
+name = "Hirdesh"
+age = 22
+salary = 100000.99
+age2 = age
+
+print(age)
+print(salary)
+print("My name is : ",name)
+print("My age is : ",age)
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(salary))
