9fa1bdadfccfe0e15fb4c7dc5d86e3a36a7fd966
Hirdesh <hirdeshk976@gmail.com>
2024-10-26 00:24:58 +0530

    Create script showcasing basic operations and comment usage in Python

---
+++ basic_operations_with_comments.py
@@ -0,0 +1,31 @@
+# Assigning value 1000 to variable 'a'
+a = 1000
+# Assigning value 500 to variable 'b'
+b = 500
+# Calculating the sum of 'a' and 'b' and storing it in 'sum'
+sum = a + b
+# Calculating the difference between 'a' and 'b' and storing it in 'diff'
+diff = a - b
+# Printing the value of 'sum' (expected output: 1500)
+print(sum)
+# Printing the value of 'diff' (expected output: 500)
+print(diff)
+
+###############
+# Example of using comments in code
+# Printing a greeting message
+print("hello world")
+# The following line is commented out and will not execute
+print("hello i am comment")
+# This block of code is also commented out, so none of these print statements will execute
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+
+# Multi-line comment section. The following lines are within a comment block and won’t execute
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+"""
