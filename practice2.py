4174a41450b6eb82037194143e6f6859f7a94fa1
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-11-02 23:20:00 +0530

    Added Python scripts for inputting first name and counting occurrences of '$'

---
+++ practice2.py
@@ -0,0 +1,9 @@
+# WAP to input user's first name & print itys length
+first_name = input("Enter First Name:")
+print(len(first_name))
+
+
+# WAP to find the occurence of '$' in a String
+
+str1 = "Hello $ World $, My $ name is Hird$esh Kumar $Yadav. I am from nepal $99.99."
+print(str1.count("$"))
