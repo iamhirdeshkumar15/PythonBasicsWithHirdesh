3cf8484e4f20d2cd11266cf40a472fa3f073a2e0
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-12-24 17:20:46 +0530

    Implemented program to print the first 10 even numbers in reverse order using a loop.

---
+++ CodingChallengesWithPython/3_print_even_numbers_reverse.py
@@ -0,0 +1,12 @@
+# Write a program using a loop to print the first 10 even numbers in reverse order.
+# using while loop
+num = 20
+while num > 0:
+    print(num)
+    num = num - 2
+
+
+# Another Method using for loop
+for num in range(20,0,-2):
+    print(num)
+
