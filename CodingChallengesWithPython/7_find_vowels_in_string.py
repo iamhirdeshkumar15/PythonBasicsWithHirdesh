82ac1743e71986b164de9b621a26841df46b87a2
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-12-26 20:31:30 +0530

    Added program to find and display vowels in a given string.

--- CodingChallengesWithPython/7_find_vowels_in_string.py
+++ CodingChallengesWithPython/7_find_vowels_in_string.py
@@ -1,22 +1,24 @@
 # Write a code to find vowel's in given string by user
 input_val = input("Enter String: ").lower()  # Convert input to lowercase
 vowels = ""
-# if "a" in input_val:
-#     vowels = vowels + "a"
-# if "e" in input_val:
-#     vowels = vowels + "e"
-# if "i" in input_val:
-#     vowels = vowels + "i"
-# if "o" in input_val:
-#     vowels = vowels + "o"
-# if "u" in input_val:
-#     vowels = vowels + "u"
-
-# print("Vowels in the string:", vowels)
-###########################################################
-# Check for vowels in the input string
-for vowel in "aeiou":
-    if vowel in input_val:
-        vowels += vowel
+if "a" in input_val:
+    vowels = vowels + "a"
+if "e" in input_val:
+    vowels = vowels + "e"
+if "i" in input_val:
+    vowels = vowels + "i"
+if "o" in input_val:
+    vowels = vowels + "o"
+if "u" in input_val:
+    vowels = vowels + "u"
 
 print("Vowels in the string:", vowels)
+
+# Using loop 
+# Check for vowels in the input string
+vowels1 = ""
+for vowel in "aeiou":
+    if vowel in input_val:
+        vowels1 += vowel
+        
+print("Vowels in the string:", vowels1)
