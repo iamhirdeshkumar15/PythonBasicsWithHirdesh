0b5955179eca765caba246207aaaac096d21da33
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-12-27 23:26:55 +0530

    Added program to encode and decode a string using a basic encoding method.

---
+++ CodingChallengesWithPython/8_encode_decode_string.py
@@ -0,0 +1,19 @@
+# Write a code to encode and decode a string.
+# Sender
+alphabets = 'abcdefghijklmnopqrstuvwxyz'
+decode = input()
+encode = ''
+for i in range (len(decode)):
+    encode += alphabets[(alphabets.index(decode[i])+3)%26]
+print(encode)
+
+
+
+# Receiver
+encode = input()
+decode = ''
+for i in range (len(encode)):
+    decode += alphabets[(alphabets.index(encode[i])-3)%26]
+print(decode)
+
+
