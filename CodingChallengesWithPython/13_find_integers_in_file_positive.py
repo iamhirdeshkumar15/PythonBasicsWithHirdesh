bee806796f0601998003a03800c403f4301f2dde
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-12-29 11:22:33 +0530

    Added program to extract all integer values from a file.

--- CodingChallengesWithPython/13_find_integers_in_file_positive.py
+++ CodingChallengesWithPython/13_find_integers_in_file_positive.py
@@ -1,7 +1,7 @@
 # Write a code to find all integer values from a file and calculate their sum.
 
 total = 0
-with open('13_find_integers_in_file_and_sum_it.txt','r') as file:
+with open('CodingChallengesWithPython/13_find_integers_in_file_and_sum_it.txt','r') as file:
     for line in file:
         words = line.split()
         for word in words:
