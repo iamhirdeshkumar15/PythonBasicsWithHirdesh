09d80805a52a4efa36f60138fe4062ae3424ad1e
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-12-29 18:20:51 +0530

    Added program to demonstrate user input handling in Python.

---
+++ CodingChallengesWithPython/15_user_input_handling.py
@@ -0,0 +1,13 @@
+# Write a code How to take input from user in python.
+
+name = input("Enter Your Name: ")
+print("Hello", name)
+print(type(name))
+
+###########################################
+
+# Taking input from the user as integer
+
+num = int(input("Enter a number: "))
+print(num)
+print(type(num))
