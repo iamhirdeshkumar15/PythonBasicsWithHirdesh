79a5aea7d85f714b68c55bc4e7eafab6e79455da
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2024-12-31 09:16:58 +0530

    Added program to demonstrate the use of single, double, and triple quotes in Python.

---
+++ CodingChallengesWithPython/17_using_quotes_in_python.py
@@ -0,0 +1,11 @@
+# Write a code for use of single, double, and triple quotes in Python.
+
+# Single (' ')
+# Double (" ")
+# Triple (''' ''')
+
+a = print('''Should I do Higher Studies
+        Should I do Job
+        Should i apply GATE Exam''')
+
+print(a)
